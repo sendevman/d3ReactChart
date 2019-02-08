f48fc3847f93d34e4f228be35cc5331ab042bb86
sendevman <devman.sd@gmail.com>
2019-02-08 16:29:47 -0300

    last finish

--- src/components/HBarChart/index.js
+++ src/components/HBarChart/index.js
@@ -23,6 +23,12 @@ class HBarChart extends Component {
 				.paddingInner(0.2)
 				.paddingOuter(0.2);
 
+		const yLabel = d3
+				.scaleBand()
+				.range([0, svgHeight])
+				.paddingInner(0.2)
+				.paddingOuter(0.2);
+
 		const xAxis = d3
 				.axisBottom()
 				.scale(x);
@@ -30,6 +36,12 @@ class HBarChart extends Component {
 		const yAxis = d3
 				.axisLeft()
 				.scale(y)
+				.tickSize(0)
+				.tickPadding(6);
+
+		const yLabelAxis = d3
+				.axisLeft()
+				.scale(yLabel)
 				.tickSize(0)
 				.tickPadding(6);
 
@@ -46,14 +58,15 @@ class HBarChart extends Component {
 			value: d.value,
 		}))
 		.then(data => {
-			x.domain(d3.extent(data, d => parseInt(d.value))).nice()
-			y.domain(data.map(d => `${d.name} ${d.value}`))
+			x.domain(d3.extent(data, d => parseInt(d.value))).nice();
+			y.domain(data.map(d => d.name));
+			yLabel.domain(data.map(d => d.value));
 			svg.selectAll('.bar')
 				.data(data)
 				.enter().append('rect')
 				.attr('class', d => 'bar bar--' + (d.value < 0 ? 'negative' : 'positive'))
 				.attr('x', d => x(Math.min(0, d.value)))
-				.attr('y', d => y(`${d.name} ${d.value}`))
+				.attr('y', d => y(d.name))
 				.attr('width', d => Math.abs(x(d.value) - x(0)))
 				.attr('height', y.bandwidth);
 
@@ -69,22 +82,26 @@ class HBarChart extends Component {
 				.selectAll('text').remove();
 
 			svg.append('g')
-				.attr('class', 'yLabel axis')
-				.attr('transform', 'translate(0, 0)')
+				.attr('class', 'ylabelL axis')
+				.attr('transform', 'translate(-30, 0)')
 				.call(yAxis)
 				.call(g => g.select('.domain').remove());
 
-			svg.selectAll('g.yLabel .tick')
-				.filter(d => parseInt(d.slice(2)) > 0)
+			svg.append('g')
+				.attr('class', 'ylabel axis')
+				.attr('transform', 'translate(0, 0)')
+				.call(yLabelAxis)
+				.call(g => g.select('.domain').remove());
+
+			svg.selectAll('g.ylabel .tick')
+				.filter(d => parseInt(d) > 0)
 				.select('text')
 				.style('color', 'steelblue')
-				.style('font-size', '14px');
 
-			svg.selectAll('g.yLabel .tick')
-				.filter(d => parseInt(d.slice(2)) <= 0)
+			svg.selectAll('g.ylabel .tick')
+				.filter(d => parseInt(d) <= 0)
 				.select('text')
 				.style('color', 'darkorange')
-				.style('font-size', '14px');;
 		});
 	}
 
@@ -106,7 +123,7 @@ HBarChart.defaultProps = {
 	data: null,
   width: 1000,
 	height: 600,
-	margin: { top: 20, right: 5, bottom: 20, left: 60 },
+	margin: { top: 20, right: 5, bottom: 20, left: 65 },
 };
 
 export default HBarChart;
