c94bc257971fb8a54fcad3c358201d505e19f01b
nativelocation <bluesky6460@mail.com>
2019-02-10 17:09:52 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 import HBarChart from './components/HBarChart';
 import RidgelineChart from './components/RidgelineChart';
+import SlopegraphChart from './components/SlopegraphChart';
 import './App.css';
 import tdata from './data.tsv';
 import cdata from './data.csv';
+import sdata from './sdata.csv';
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
         <header className="App-header">
           <HBarChart data={tdata} />
           <RidgelineChart data={cdata} />
+          <SlopegraphChart data={sdata} />
         </header>
       </div>
     );
